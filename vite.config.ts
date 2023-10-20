c53761322551a0a30635e2f55fcadc4306f0730e
Mahmoudamin11 <mahmoudamin123321@gmail.com>
2023-10-20 07:47:57 +0200

    trying to edit vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/article-preview-frontendMentorChallenge/',
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/article-preview-frontendMentorChallenge/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+
+  return config
 })
